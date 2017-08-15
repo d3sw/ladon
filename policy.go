3eb3f14d63c8ff43475dc4d8072bcba8cec43710
ShuoYen.Lin <Shuoyen.Lin@bydeluxe.com>
2017-08-15 14:49:27 -0700

    extend DefaultPolicy & add rdb manager

--- policy.go
+++ policy.go
@@ -2,8 +2,14 @@ package ladon
 
 import (
 	"encoding/json"
+	"fmt"
+	"strings"
 
 	"github.com/pkg/errors"
+)
+
+var (
+	ErrPolicyNotFound = errors.New("policy not found")
 )
 
 // Policies is an array of policies.
@@ -131,4 +137,107 @@ func (p *DefaultPolicy) GetEndDelimiter() byte {
 // GetStartDelimiter returns the delimiter which identifies the beginning of a regular expression.
 func (p *DefaultPolicy) GetStartDelimiter() byte {
 	return '<'
+}
+
+// HasIdentity returns true if the provided identity is part of the policy i.e. contained
+// in the subject.
+func (p *DefaultPolicy) HasIdentity(id string) bool {
+	for _, sub := range p.Subjects {
+		if id == sub {
+			return true
+		}
+	}
+
+	return false
+}
+
+// AttachIdentity adds identities to the Subjects
+func (p *DefaultPolicy) AttachIdentity(ids ...string) error {
+	for _, id := range ids {
+		if p.HasIdentity(id) {
+			return fmt.Errorf("identity already attached: %s", id)
+		} else if err := checkIdentity(id); err != nil {
+			return err
+		}
+
+		p.Subjects = append(p.Subjects, id)
+	}
+	return nil
+}
+
+func (p *DefaultPolicy) detachIdentity(id string) error {
+	for i, sub := range p.Subjects {
+		if id == sub {
+			if len(p.Subjects) == 1 {
+				p.Subjects = []string{}
+			} else {
+				p.Subjects = append(p.Subjects[:i], p.Subjects[i+1:]...)
+			}
+			return nil
+		}
+	}
+
+	return fmt.Errorf("identity not attached: %s", id)
+}
+
+// DetachIdentity removes a user,group,role or any other identity from the subject of the
+// policy
+func (p *DefaultPolicy) DetachIdentity(ids ...string) error {
+	for _, id := range ids {
+		if err := p.detachIdentity(id); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Validate validates properties and values of the policy
+func (p *DefaultPolicy) Validate() error {
+	err := p.ValidateSubjects()
+	if err == nil {
+		err = p.ValidateEffect()
+	}
+	return err
+}
+
+// ValidateSubjects validates each subject to make sure they are syntactically correct
+func (p *DefaultPolicy) ValidateSubjects() error {
+	//
+	// TODO:
+	// for _, sub := range p.Subjects {
+	// 	if parts := strings.Split(sub, ":"); len(parts) < 2 {
+	// 		return fmt.Errorf("ambigous subject: %s", sub)
+	// 	}
+	// }
+
+	if p.Subjects == nil || len(p.Subjects) < 1 {
+		return fmt.Errorf("subject missing")
+	}
+	return nil
+}
+
+// ValidateEffect validates the effect
+func (p *DefaultPolicy) ValidateEffect() error {
+	if p.Effect != AllowAccess && p.Effect != DenyAccess {
+		return fmt.Errorf("unsupported effect: %s", p.Effect)
+	}
+	return nil
+}
+
+func (p *DefaultPolicy) CheckID() (string, string, error) {
+	id := p.GetID()
+	parts := strings.Split(id, ".")
+	if len(parts) < 2 {
+		return "", "", errors.New("invalid id format")
+	}
+
+	return parts[0], strings.Join(parts[1:], "."), nil
+}
+
+func checkIdentity(id string) error {
+	if p := strings.Split(id, ":"); len(p) < 2 {
+		return fmt.Errorf("invalid id: %s", id)
+	}
+
+	return nil
 }
--- warden.go
+++ warden.go
@@ -1,5 +1,12 @@
 package ladon
 
+import (
+	"net/http"
+
+	"github.com/SermoDigital/jose/jwt"
+	"github.com/d3sw/fuac/foken"
+)
+
 // Request is the warden's request object.
 type Request struct {
 	// Resource is the resource that access is requested to.
@@ -22,4 +29,55 @@ type Warden interface {
 	//    return errors.New("Not allowed")
 	//  }
 	IsAllowed(r *Request) error
+}
+
+// BuildPolicyRequest builds a PolicyRequest from the http.Request.  It sets all fields
+// in claims, http headers, and http query pararms to the context
+func BuildRequest(r *http.Request, claims jwt.Claims) *Request {
+	req := &Request{
+		Subjects: []string{},
+		Action:   r.Method,
+	}
+
+	ctx := Context{"remoteIP": r.RemoteAddr}
+
+	// Set claims to context
+	if claims != nil {
+		for k, v := range claims {
+			ctx[k] = v
+		}
+	}
+
+	// Add user:email as subject
+	eml := claims.Get(foken.ClaimEmail)
+	if email, ok := eml.(string); ok {
+		req.Subjects = append(req.Subjects, "user:"+email)
+		delete(ctx, foken.ClaimEmail)
+	}
+	// Add user:username as subject
+	puser := claims.Get(foken.ClaimUsername)
+	if username, ok := puser.(string); ok {
+		req.Subjects = append(req.Subjects, "user:"+username)
+		delete(ctx, foken.ClaimUsername)
+	}
+
+	//
+	// TODO: add role subjects
+	//
+
+	// Set headers as context
+	for k := range r.Header {
+		ctx[k] = r.Header.Get(k)
+	}
+	delete(ctx, "Authorization")
+
+	// Set query params as context
+	vals := r.URL.Query()
+	for k, v := range vals {
+		ctx[k] = v
+	}
+
+	req.Context = ctx
+
+	return req
 }
